172f4834e3c49a1badb1268c2b0ce900c7b665b9
ednow <3334300267@qq.com>
2021-06-24 11:24:20 +0800

    :art: 重写部分测试代码

--- PTA/1002/A_plus_B_for_Polynomials.py
+++ PTA/1002/A_plus_B_for_Polynomials.py
@@ -4,7 +4,7 @@ from typing import Dict
 
 def rectify_str(_str: str) -> Dict:
     """
-    给定一个题目定义的多项式字符串,将其放到一个dict中{幂，系数}
+    给定一个题目定义的多项式字符串,将其放到一个dict中{幂:系数}
     :param _str: 题目定义的多项式字符串
     """
     result = {}
@@ -21,8 +21,8 @@ def rectify_data(data: Dict) -> str:
     """
     data = {k: data[k] for k in sorted(data.keys(), reverse=True)}
 
-    if len(data.keys()) > 0:
-        return f"{len(data.keys())} {' '.join(map(str ,sum(zip(data.keys(), data.values()), ())))}"
+    if len(data) > 0:
+        return f"{len(data)} {' '.join(map(str ,sum(zip(data.keys(), data.values()), ())))}"
     else:
         return "0"
 
@@ -36,7 +36,7 @@ def cal_poly(a: Dict, b: Dict):
     result = {}
     a_keys = list(a.keys())
     b_keys = list(b.keys())
-    for e in range(max(a_keys + b_keys) + 1):
+    for e in set(a_keys + b_keys):
         if (e in a_keys) and (e in b_keys):
             c = round(a[e] + b[e], 1)
             if c != 0:
@@ -58,13 +58,16 @@ class Test(unittest.TestCase):
     def test_solution(self):
         testCases = [
             ["3 2 1.5 1 2.9 0 3.2", "2 1 2.4 0 3.2", "2 2 1.5 1 0.5"],
-            ["0", "1 2 -1", "1 2 1"],  # 格式错误的代码
-            ["1 1 -1.0", "1 2 0", "1 1 -1"],  # 极端情况1, 解决了, 并未ac
-            ["0", "1 1 0", "1 2 0"]  # 极端情况1, 解决了, 并未ac
+            ["0", "1 2 -1", "1 2 1"],  # 某个测试点的输出格式错误的测试数据
+            ["1 1 -1.0", "1 2 0", "1 1 -1"],  # 极端情况1, 解决了, 但未过测试点1
+            ["0", "1 1 0", "1 2 0"],  # 极端情况1, 解决了, 但未过测试点1
+            # 极端情况2, 64位整型溢出, 解决了, 但未过测试点1
+            ["1 18446744073709551616 1.0", "1 18446744073709551616 0.5", "1 18446744073709551616 0.5"]
         ]
 
-        for result, a, b in testCases:
-            assert result == rectify_data(cal_poly(rectify_str(a), rectify_str(b)))
+        for answer, a, b in testCases:
+            result = rectify_data(cal_poly(rectify_str(a), rectify_str(b)))
+            assert answer == result, f"{answer}, {result}"
 
 
 if __name__ == '__main__':
--- PTA/1002/README.md
+++ PTA/1002/README.md
@@ -1,7 +1,16 @@
 # 总结
 
 ## python
+[一个正确答案](https://blog.csdn.net/weixin_44575329/article/details/104362066)
+
+### 关于四舍五入
+[python中四舍五入的特殊情况](https://zhuanlan.zhihu.com/p/93363837)
+
+[参考文献2](https://zhuanlan.zhihu.com/p/115431517)
+
+
 ### 思路
 首先处理题目给的字符串，转为自己中间
 
 ### 语法技术
+#### keys()不能进行+运算

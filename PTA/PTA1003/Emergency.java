2ce21dfb83a16580804277c24228960fed6cb786
ednow <3334300267@qq.com>
2021-06-25 00:03:06 +0800

    save

--- PTA/PTA1003/Emergency.java
+++ PTA/PTA1003/Emergency.java
@@ -1,7 +1,22 @@
 package PTA.PTA1003;
 
-interface Graph {
+import java.util.*;
 
+interface Graph {
+    // 判断图G是否存在边<x, y>或(x, y)。
+    boolean adjacent();
+
+    // 列出图G中与结点x邻接的边
+    List<Integer> Neighbors();
+
+    // 在图G中插入顶点x
+    boolean InsertVertex();
+
+    // 从图G中删除顶点x
+    boolean DeleteVertex();
+
+    // 若无向边(x, y)或有向边<x, y>存在，则从图G中删除该边
+    boolean RemoveEdge();
 }
 
 

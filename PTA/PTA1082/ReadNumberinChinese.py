e4b2278b068e9d0e5c482cab397fefd73240f3b5
ednow <3334300267@qq.com>
2021-07-06 23:30:35 +0800

    重构中间数据结构 for pta1082.python

--- PTA/PTA1082/ReadNumberinChinese.py
+++ PTA/PTA1082/ReadNumberinChinese.py
@@ -5,27 +5,29 @@ def split_digits_by_four(num: str) -> List[str]:
     return [num[i:i+4] for i in range(0, len(num), 4)]
 
 
-def read_four_digits(num: str) -> str:
+def read_four_digits(num: str) -> List[str]:
     """
     :param num: 阶序：个十百千
     """
     numToChinese = ["ling", "yi", "er", "san", "si", "wu", "liu", "qi", "ba", "jiu"]
     units = ["Shi", "Bai", "Qian"]
-    result = ""
+    result = []
 
     zero = False
     if num[0] == '0':
         zero = True
     else:
-        result = " " + numToChinese[int(num[0])] + result
+        result.insert(0, numToChinese[int(num[0])])
 
     for idx, one in enumerate(num[1:]):
         # 如果是连续0，将不做任何处理
         if not zero and one == '0':  # 如果上一个不是0，这一位是0，这时候需要把0读出来;
-            result = " " + "ling" + result
+            # result = " " + "ling" + result
+            result.insert(0, "ling")
             zero = True  # 状态变化
         elif one != '0':  # 只要不是0都要读出来，并且加上单位
-            result = " " + numToChinese[int(one)] + " " + units[idx] + result
+            result.insert(0, units[idx])
+            result.insert(0, numToChinese[int(one)])
             zero = False  # 状态变化
 
     # # 如果是连续0，将不做任何处理
@@ -58,11 +60,11 @@ def summit():
     else:
         num = string
 
-    result = ""
+    result = []
     for idx, group in enumerate(split_digits_by_four("".join(reversed(num)))):
-        result = read_four_digits(group) + " " + units[idx] + result
-    result = sign + result
-    print(result.strip())
+        result = read_four_digits(group) + [units[idx]] + result
+    result = [sign] + result
+    print(" ".join(result).strip())
 
 
 if __name__ == '__main__':

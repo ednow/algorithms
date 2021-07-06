ce6239c0904a256c0896584a31fb6e8f47d6c048
ednow <3334300267@qq.com>
2021-07-06 21:52:39 +0800

    pta1073.python  ac

--- PTA/PTA1073/README.md
+++ PTA/PTA1073/README.md
@@ -24,3 +24,13 @@
 </details>
 
 测试点4
+
+
+### 第二次提交
+<details>
+    <summary>Details</summary>
+
+
+![](https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210706215104.png)
+</details>
+
--- PTA/PTA1073/ScientificNotation.py
+++ PTA/PTA1073/ScientificNotation.py
@@ -1,16 +1,4 @@
-import re
-
-
-# def find_first_not_zero(num: str):
-#     search = re.search(r"[^0]", num)
-#     if search is not None:  # 如果最后呈现000xxx
-#         return search.start()
-
-
 def summit():
-    """
-
-    """
     string = input()
     numSign = string[0]
     numSign = '' if numSign == '+' else numSign
@@ -26,23 +14,14 @@ def summit():
             result = numSign + integer + fractional + '0' * zeroToAdd
         elif len(fractional) == exp:  # 右移位数刚好等于小数部分的长度
             result = integer + fractional
-            # 根据题意下面的情况不可能出现
-            # if re.search(r"[^0]", result) is not None:  # 如果最后呈现000xxx
-            #     result = result[re.search(r"[^0]", result).start():]  # 去掉前面的0使其变为xxx
             result = numSign + result
         else:  # 右移位数小于小数部分的长度
             floatPos = 1 + exp  # 小数点右移后的位置
             num = num.replace(".", "")
-            integer, fractional = num[:floatPos+1], num[floatPos:]
-            # 根据题意下面的情况不可能出现
-            # if re.search(r"[^0]", integer) is not None:  # 如果整数部分呈现呈现000xxx
-            #     integer = integer[re.search(r"[^0]", integer).start():]  # 去掉前面的0使其变为xxx
-            # else:  # 整数部分全是0
-            #     integer = '0'
+            integer, fractional = num[:floatPos], num[floatPos:]
             result = numSign + integer + "." + fractional
     elif exp < 0:
         zeroToAdd = - exp - 1
-        # integer, fractional = num[0], num[2:]
         num = num.replace(".", "")
         result = numSign + '0' + '.' + '0' * zeroToAdd + num
     print(result)

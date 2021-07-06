296c1df52c297f9409aa8517a68363c35e833245
ednow <3334300267@qq.com>
2021-07-06 17:30:47 +0800

    :hammer: 重构测试架构和main.py的结构

--- PTA/PTA1073/ScientificNotation.py
+++ PTA/PTA1073/ScientificNotation.py
@@ -1,8 +1,3 @@
-import unittest
-import sys
-from io import StringIO
-
-
 def summit():
     """
 
@@ -18,27 +13,6 @@ def summit():
         pass
     elif exp < 0:
         pass
-
-class TestPTA1061(unittest.TestCase):
-    def setUp(self) -> None:
-        import json
-        with open("data.json", encoding="utf-8", mode="r") as f:
-            self.testCases = json.load(f)
-
-        with open("config.json", encoding="utf-8", mode="r") as f:
-            self.config = json.load(f)
-
-        if any(list(self.config.values())):
-            self.testCases = list(filter(lambda x: x["id"] == self.config["testCaseId"], self.testCases))
-
-    def test_pta1061(self):
-        for idx, testCase in enumerate(self.testCases):
-            a, answer = testCase["data"], testCase["answer"]
-            sys.stdin = StringIO(a)
-            f = StringIO()
-            sys.stdout = f
-            summit()
-            assert answer == f.getvalue(), f"{testCase['id']}right:{answer}, my:{f.getvalue()}"
 
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -1,38 +1,53 @@
-import unittest
 import importlib
 import os
 import sys
 import json
 from io import StringIO
 from utils.Exception import *
+import logging
 
-my_name = 'PTA'
-my_spec = importlib.util.spec_from_loader(my_name, loader=None)
-my_module = importlib.util.module_from_spec(my_spec)
-# my_code = ""
-with open(os.path.join("PTA", "PTA1061", "Dating.py"), "r", encoding="utf-8") as f:
-    my_code = f.read()
 
-with open(os.path.join("PTA", "PTA1061", "data.json"), encoding="utf-8", mode="r") as f:
-    testCases = json.load(f)
+class Test:
+    def __init__(self):
+        with open("config.json", "r", encoding="utf-8") as f:
+            self.config = json.load(f)
+        self.testCases = None  # 测试用例的信息
+        self.entry = None  # 模块的入口函数
+        self.set_up()
 
-exec(my_code, my_module.__dict__)
-sys.modules['my_module'] = my_module
+    def set_up(self):
+        # 如果未指定data.json的位置默认就是模块所在的位置
+        dataPath = self.config["dataPath"] if self.config["dataPath"] is not None \
+            else os.path.join(*self.config["module"].split(".")[:-1]+["data.json"])
 
-with open(os.path.join("PTA", "PTA1061", "config.json"), encoding="utf-8", mode="r") as f:
-    config = json.load(f)
+        with open(dataPath, encoding="utf-8", mode="r") as f:
+            self.testCases = json.load(f)
 
-if any(list(config.values())):
-    testCases = list(filter(lambda x: x["id"] == config["testCaseId"], testCases))
+        # 如果指定了test case id，则只测试对应id的测试用例
+        if self.config["testCaseId"] is not None:
+            self.testCases = list(filter(lambda x: x["id"] == self.config["testCaseId"], self.testCases))
 
-for idx, testCase in enumerate(testCases):
-    a, answer = testCase["data"], testCase["answer"]
-    sys.stdin = StringIO(a)
-    f = StringIO()
-    stdoutBackup = sys.stdout
-    sys.stdout = f
-    my_module.summit()
-    assert answer == f.getvalue(), repr(f"error in id={testCase['id']},expected:{answer}, however:{f.getvalue()}")
-    sys.stdout = stdoutBackup
-    print(f"pass:id = {testCase['id']}")
+        # 如果设定了模块的入口函数
+        if self.config["moduleEntry"] is not None:
+            self.entry = self.config["moduleEntry"]
+        else:
+            self.entry = "summit"
 
+    def run(self):
+        module = importlib.import_module(self.config["module"])
+        print(f"你正在测试的代码为：{self.config['module']}.py")
+        for idx, testCase in enumerate(self.testCases):
+            a, answer = testCase["data"], testCase["answer"]
+            sys.stdin = StringIO(a)
+            f = StringIO()
+            stdoutBackup = sys.stdout
+            sys.stdout = f
+            getattr(module, self.entry)()
+            assert answer == f.getvalue(), repr(
+                f"error in id={testCase['id']},expected:{answer}, however:{f.getvalue()}")
+            sys.stdout = stdoutBackup
+            print(f"pass:id = {testCase['id']}")
+
+
+if __name__ == '__main__':
+    Test().run()

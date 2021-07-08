e936cfb84319ecf0cec68afcc3a7a2704805f739
ednow <3334300267@qq.com>
2021-07-08 13:07:53 +0800

    pta1152.python超时

---
+++ PTA/PTA1152/GoogleRecruitment.py
@@ -0,0 +1,26 @@
+def is_prime(num: int) -> bool:
+    for i in range(2, num-1):
+        if num % i == 0:
+            return False
+    return True
+
+
+def summit():
+    numLen, primeLen = map(int, input().split())
+    num = input()
+    find = False
+    result = 0
+    for i in range(numLen - primeLen + 1):
+        result = num[i: i + primeLen]
+        if is_prime(int(result)):
+            find = True
+            break
+
+    if find:
+        print(result)
+    else:
+        print("404")
+
+
+if __name__ == '__main__':
+    summit()
---
+++ PTA/PTA1152/README.md
@@ -0,0 +1,31 @@
+# 1152总结
+## 考点
++ 质数
+
+## 问题重述
+
+<details>
+    <summary>Details</summary>
+
+![](https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210708130534.png)
+</details>
+
+质数的定义：质数是指在大于1的自然数中，除了1和它本身以外不再有其他因数的自然数。
+
+0既不是质数也不是合数
+
+1是质数
+## python
+
+### 第一次提交
+最后一个测试点超时
+
+<details>
+    <summary>Details</summary>
+
+
+![](https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210708130634.png)
+
+</details>
+
+

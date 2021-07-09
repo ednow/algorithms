36271ebf3f9480a2e34995a43ed94ba0d7f3f7c2
ednow <3334300267@qq.com>
2021-07-10 00:01:04 +0800

    pta1032.cpp ac

--- PTA/PTA1074/ReversingLinkedList.py
+++ PTA/PTA1074/ReversingLinkedList.py
@@ -1,5 +1,15 @@
+from typing import List, Dict
+
+
 def summit():
-    pass
+    head, nodeNum, step = input().split()
+    memory: Dict[str, str] = {}
+    link1: List[str] = []
+
+    # 生成内存块
+    for _ in range(int(nodeNum)):
+        address, _, pointer = input().split()
+        memory[address] = pointer
 
 
 if __name__ == '__main__':

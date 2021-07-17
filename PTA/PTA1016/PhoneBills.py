1b4e0d663673970017cb69d0f8bd4642d98e51cf
laptop <3334300267@qq.com>
2021-07-17 21:48:47 +0800

    磨pta1016.python: 修完bug

--- PTA/PTA1016/PhoneBills.py
+++ PTA/PTA1016/PhoneBills.py
@@ -24,79 +24,88 @@ def summit():
             key=lambda x: (x[0][1], x[0][2], x[0][3])
         )
 
-        onLine = -1
-        # 找到第一条onLine的记录
         # off-line
         # on-line
-        for idx in range(len(records[one])):
-            if records[one][idx][1] == 'on-line':
-                onLine = idx
-                break
-
-        if onLine == -1:  # 全是off-line，不合法的记录
-            continue
-        else:
-            for idx in range(onLine + 1, len(records[one])):
-                if records[one] == 'on-line':  # 上一天on-line没有pair off-line
+        start = 0
+        while start != len(records[one])-1:
+            onLine = -1
+            offline = -1
+            # 找到第一条onLine的记录
+            for idx in range(start, len(records[one])):
+                if records[one][idx][1] == 'on-line':
                     onLine = idx
-                else:  # 计算账单
-                    bill = 0  # 账单
-                    last = 0  # 持续时间
-                    oneDetail = bills.get(one, {})  # 某个人的账单列表，总的账单
-                    oneBill = oneDetail.get("bills", [])  # 账单列表
-                    oneTotal = oneDetail.get("total", 0)  # 总的账单
+                    break
 
-                    # 如果在同一天
-                    if records[one][onLine][0][1] == records[one][idx][0][1]:
-                        if records[one][onLine][0][2] == records[one][idx][0][2]:  # 如果在同一个小时
-                            last += records[one][idx][0][3] - records[one][onLine][0][3]
-                            bill += last * rates[records[one][idx][0][2]] / 100
-                        else:  # 不在同一个小时
-                            # 先求出on-line的小时，到该小时结束花的钱
+            if onLine == -1:  # 全是off-line，不合法的记录
+                continue
+            else:
+                for idx in range(onLine + 1, len(records[one])):
+                    if records[one][idx][1] == 'on-line':  # 上一天on-line没有pair off-line
+                        onLine = idx
+                    else:  # 计算账单
+                        offline = idx
+                        bill = 0  # 账单
+                        last = 0  # 持续时间
+                        oneDetail = bills.get(one, {})  # 某个人的账单列表，总的账单
+                        oneBill = oneDetail.get("bills", [])  # 账单列表
+                        oneTotal = oneDetail.get("total", 0)  # 总的账单
+
+                        # 如果在同一天
+                        if records[one][onLine][0][1] == records[one][idx][0][1]:
+                            if records[one][onLine][0][2] == records[one][idx][0][2]:  # 如果在同一个小时
+                                last += records[one][idx][0][3] - records[one][onLine][0][3]
+                                bill += last * rates[records[one][idx][0][2]] / 100
+                            else:  # 不在同一个小时
+                                # 先求出on-line的小时，到该小时结束花的钱
+                                temp = 60 - records[one][onLine][0][3]
+                                bill += temp * rates[records[one][onLine][0][2]]
+                                last += temp
+                                # 再求出off-line的时候，花的钱
+                                bill += records[one][idx][0][3] * rates[records[one][idx][0][2]]
+                                last += records[one][idx][0][3]
+                                # 求出中间小时花的钱
+                                for hour in range(records[one][onLine][0][2] + 1, records[one][idx][0][2]):
+                                    bill += 60 * rates[hour]
+                                    last += 60
+                        else:  # 不在同一天
+                            # 把on-line 那一天的钱结了
+                            # 计算on-line 那个小时的钱
                             temp = 60 - records[one][onLine][0][3]
                             bill += temp * rates[records[one][onLine][0][2]]
                             last += temp
-                            # 再求出off-line的时候，花的钱
-                            bill += records[one][idx][0][3] * rates[records[one][idx][0][2]]
-                            last += records[one][idx][0][3]
-                            # 求出中间小时花的钱
-                            for hour in range(records[one][onLine][0][2] + 1, records[one][idx][0][2]):
-                                bill += 60 * rates[hour]
+                            # 结算那一天剩下的小时
+                            for hour in range(records[one][onLine][0][2] + 1, 24):
                                 last += 60
-                    else:  # 不在同一天
-                        # 把on-line 那一天的钱结了
-                        # 计算on-line 那个小时的钱
-                        temp = 60 - records[one][onLine][0][3]
-                        bill += temp * rates[records[one][onLine][0][2]]
-                        last += temp
-                        # 结算那一天剩下的小时
-                        for hour in range(records[one][onLine][0][2] + 1, 24):
-                            last += 60
-                            bill += 60 * rates[hour]
-                        # 把off-line 那一天的钱结了
-                        last += records[one][idx][0][3]
-                        bill += records[one][idx][0][3] * rates[records[one][idx][0][2]]
-                        for hour in range(0, records[one][idx][0][2]):
-                            last += 60
-                            bill += 60 * rates[hour]
-                        # 把中间天的钱结了
-                        for _ in range(records[one][onLine][0][1], records[one][idx][0][1]):
-                            last += 24 * 60
-                            bill += sum(rates) * 60
-                    oneBill.append(
-                            ":".join([f"{i:02d}" for i in records[one][onLine][0]]) + ' ' +
-                            ":".join([f"{i:02d}" for i in records[one][idx][0]]) + ' ' +
-                            str(last) + ' ' +
-                            '$' + f"{bill:.2f}"
-                    )
-                    oneDetail.update({
-                        "bills": oneBill
-                    })
-                    oneDetail.update({
-                        "total": oneTotal + bill
-                    })
-                    bills[one] = oneDetail
-
+                                bill += 60 * rates[hour]
+                            # 把off-line 那一天的钱结了
+                            last += records[one][idx][0][3]
+                            bill += records[one][idx][0][3] * rates[records[one][idx][0][2]]
+                            for hour in range(0, records[one][idx][0][2]):
+                                last += 60
+                                bill += 60 * rates[hour]
+                            # 把中间天的钱结了
+                            for _ in range(records[one][onLine][0][1] + 1, records[one][idx][0][1]):
+                                last += 24 * 60
+                                bill += sum(rates) * 60
+                        bill /= 100
+                        oneBill.append(
+                                ":".join([f"{i:02d}" for i in records[one][onLine][0][1:]]) + ' ' +
+                                ":".join([f"{i:02d}" for i in records[one][idx][0][1:]]) + ' ' +
+                                str(last) + ' ' +
+                                '$' + f"{bill:.2f}"
+                        )
+                        oneDetail.update({
+                            "bills": oneBill
+                        })
+                        oneDetail.update({
+                            "total": oneTotal + bill
+                        })
+                        bills[one] = oneDetail
+                        break
+            if offline != -1:
+                start = offline
+            else:  # 这个人的记录全是online的记录
+                break
     # 得到账单的月份
     month = 0
     for record in records.values():
@@ -106,11 +115,11 @@ def summit():
     # 对账单进行字母表排序
     bills = sorted(bills.items(), key=lambda x:x[0])
     for one, oneBills in bills:
-        result += one + ' ' + f"{month:02d}" + \
+        result += one + ' ' + f"{month:02d}" + "\n" + \
                 "\n".join(oneBills["bills"]) + "\n" + \
-                str(oneBills["total"]) + "\n"
+                "Total amount: " + f"${oneBills['total']:.2f}" + "\n"
 
-    print(result)
+    print(result, end='')
 
 
 if __name__ == '__main__':

4d6fb40b93ab2bfbeeb0186ff14141da9d5acb32
ednow <3334300267@qq.com>
2021-08-05 00:13:57 +0800

    pta1055 通过样例

--- PTA/PTA1055/TheWorldsRichest.py
+++ PTA/PTA1055/TheWorldsRichest.py
@@ -10,8 +10,8 @@ def summit():
 
     # 读入记录
     for _ in range(N):
-        strings = input()
-        print(strings)
+        strings = input().split()
+        # print(strings)
         records.append([strings[0], int(strings[1]), int(strings[2])])
 
     # 读入请求
@@ -19,16 +19,16 @@ def summit():
         queries.append(list(map(int, input().split())))
 
     for idx, query in enumerate(queries):
-        raw = list(filter(lambda x: query[1] < x[1] < query[2], records))
+        raw = list(filter(lambda x: query[1] <= x[1] <= query[2], records))
         # 三个关键字的先后顺序的原理详见数据结构习题
         # 先根据年龄和姓名非降序排序
         raw.sort(key=lambda x: (x[1], x[0]))
         # 再根据净利润降序排序
         raw.sort(key=lambda x: x[2], reverse=True)
-        result += f'Case #{idx + 1}\n'
+        result += f'Case #{idx + 1}:\n'
         if len(raw) != 0:  # 如果存在记录
             for _idx in range(min(len(raw), query[0])):
-                result += " ".join(raw[_idx]) + '\n'
+                result += " ".join(map(str, raw[_idx])) + '\n'
         else:
             result += 'None\n'
 

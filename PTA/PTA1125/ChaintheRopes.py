dea8be6ec570405d2b49993ebb7efcb4c2fddc4b
ednow <3334300267@qq.com>
2021-08-07 21:58:12 +0800

    pta1125.py ac:读题神坑

--- PTA/PTA1125/ChaintheRopes.py
+++ PTA/PTA1125/ChaintheRopes.py
@@ -1,29 +1,11 @@
-from typing import List
-
-
-def fold(currentLen: int, ropes: List[int]) -> int:
-    """
-    将绳子对折
-    :param currentLen: 现在对折的长度
-    :param ropes: 还没折的绳子
-    """
-    # 没有绳子了，退出递归
-    if len(ropes) == 0:
-        return currentLen
-
-    # nextLen = int((currentLen + ropes[0])/2)
-    nextLen = int(currentLen / 2) + int(ropes[0] / 2)
-    if nextLen > currentLen:
-        fold(nextLen, ropes[1:])
-    else:  # 现在的长度已经是最大了，退出递归
-        return currentLen
+from functools import reduce
 
 
 def summit():
     input()
     nums = list(map(int, input().split()))
-    nums.sort(reverse=True)
-    print(fold(int(nums[1]/2)+int(nums[0]/2), nums[2:]))
+    nums.sort()
+    print(reduce(lambda a, b: int((a+b)/2), nums[1:], nums[0]))
 
 
 if __name__ == '__main__':
--- PTA/PTA1125/README.md
+++ PTA/PTA1125/README.md
@@ -15,6 +15,7 @@
 ### 坑点
 实测有空行
 
+神坑：要把所有的绳子都折到一起
 ## python
 ### 第一次提交
 
@@ -28,3 +29,12 @@ version1
 version2
 
 <details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210807205530.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210807205530.png';" /></details>
+
+测试点1的答案5001
+
+参考文献
+
+### 最后一次提交
+reduce的起点必须是num[0]，然后从num[1:]开始reduce
+
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210807215711.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210807215711.png';" /></details>

2a3414cd4e2a461d1a538df7b21af3f16819f90c
ednow <3334300267@qq.com>
2021-08-09 00:00:23 +0800

    pta1020.python ac

---
+++ PTA/PTA1020/README.md
@@ -0,0 +1,30 @@
+# PTA1020总结
+## 考点
++ 二叉树
++ 树
+
+
+## 问题重述
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210808222132.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210808222132.png';" /></details>
+
+### 要点复述
++ 已知后序和中序求层次遍历序列
++ 第一行后序，第二行中序
+
+### 坑点
++ 实测输出结尾有空行
+
+
+## python
+
+### 思路
+
++ 切片的时候需要小心一下
+
+### 语法技术
+不能使用namedtuple，tuple是不可变的
+
+slot的使用，[参考文献](https://stackoverflow.com/questions/5943425/is-there-a-data-type-in-python-similar-to-structs-in-c)
+
+### 第一次提交
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210808235418.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210808235418.png';" /></details>
---
+++ PTA/PTA1020/TreeTraversals.py
@@ -0,0 +1,100 @@
+from typing import List
+import queue
+
+
+# 二叉树的链式存储结构
+class Node:
+    __slots__ = 'left', 'right', 'data'
+
+
+def attach_node(father: Node, postOrderList: List[int], inOrderList: List[int], isLeft: bool):
+    """
+    递归将节点附着到树上
+    :param father: 当前要附着的节点的父亲节点
+    :param postOrderList: 后序序列
+    :param inOrderList: 中序序列
+    :param isLeft: 现在准备附着的节点是附着到父亲节点左孩子还是右孩子
+    """
+    if len(postOrderList) == 0:
+        return 0
+
+    # 后序的最后一个是根节点
+    root = Node()
+    root.right = None
+    root.data = postOrderList[-1]
+    root.left = None
+
+    # 根据需求附着节点
+    if isLeft:
+        father.left = root
+    else:
+        father.right = root
+
+    # 根节点所在中序序列中的位置
+    rootIndex = inOrderList.index(postOrderList[-1])
+
+    # 根据中序左孩子的列表扫描，后序序列左孩子的位置
+    rightestOfLeft = -1  # 根节点左孩子的最右边的孩子的下标
+    for i in range(len(inOrderList[:rootIndex])):
+        if postOrderList[i] in inOrderList[:rootIndex]:
+            rightestOfLeft += 1
+
+    # 将左孩子附着到根节点
+    attach_node(root, postOrderList[0: rightestOfLeft + 1], inOrderList[:rootIndex], isLeft=True)
+    # 将右孩子附着到根节点
+    attach_node(root, postOrderList[rightestOfLeft + 1: -1], inOrderList[rootIndex + 1:], isLeft=False)
+
+
+def level_traversal(root: Node) -> List[int]:
+    """
+    根据给定的树的根节点，对树进行层次遍历
+    :param root: 树的根节点
+    """
+    q = queue.Queue()
+    result = []
+    q.put(root)
+    while not q.empty():
+        item: Node = q.get()
+        result.append(item.data)
+
+        # 压入左孩子
+        if item.left is not None:
+            q.put(item.left)
+
+        # 压入左孩子
+        if item.right is not None:
+            q.put(item.right)
+
+    return result
+
+
+def summit():
+    input()
+    postOrderList = list(map(int, input().split()))
+    inOrderList = list(map(int, input().split()))
+
+    # 后序的最后一个是根节点
+    root = Node()
+    root.right = None
+    root.data = postOrderList[-1]
+    root.left = None
+
+    # 根节点所在中序序列中的位置
+    rootIndex = inOrderList.index(postOrderList[-1])
+
+    # 根据中序左孩子的列表扫描，后序序列左孩子的位置
+    rightestOfLeft = -1  # 根节点左孩子的最右边的孩子的下标
+    for i in range(len(inOrderList[:rootIndex])):
+        if postOrderList[i] in inOrderList[:rootIndex]:
+            rightestOfLeft += 1
+
+    # 将右孩子附着到根节点
+    attach_node(root, postOrderList[0: rightestOfLeft + 1], inOrderList[:rootIndex], isLeft=True)
+    # 将右孩子附着到根节点
+    attach_node(root, postOrderList[rightestOfLeft + 1: -1], inOrderList[rootIndex + 1:], isLeft=False)
+
+    print(" ".join(map(str, level_traversal(root))))
+
+
+if __name__ == '__main__':
+    summit()

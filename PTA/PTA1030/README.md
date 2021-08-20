61eaccea6f6b256562b09f9d0ecbd2c1095c625c
ednow <3334300267@qq.com>
2021-08-20 16:01:20 +0800

    pta1030.python 第一次提交

--- PTA/PTA1030/README.md
+++ PTA/PTA1030/README.md
@@ -7,11 +7,20 @@
 <details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210819132121.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210819132121.png';" /></details>
 
 ### 要点复述
+给点节点进行dijkstra求单源最短路径，如果最短路径相同，选择花费最短的那条路
+
 
 ## 边界点
 
 ## 思路
+dijkstra还有优化的空间
 
-## cpp
+这次跟1018不同，换个思路，直接从源点开始搜.
+
+
+
+## python
+
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210820160056.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210820160056.png';" /></details>
 
 ### 语法技术
--- PTA/PTA1030/TravelPlan.py
+++ PTA/PTA1030/TravelPlan.py
@@ -1,5 +1,98 @@
+from typing import List, Dict
+
+
+def get_paths(
+        nodes: List[Dict],
+        paths: List[List[int]],
+        stack: List[int],
+        source: int,
+        destination: int
+):
+    """
+    通过dijkstra的结果信息表凭借出每一条路径
+    :param source: 现在走到的点
+    :param destination: 目标点
+    # :param cost: 得到每条路的花费 cost可以在外部求
+    :param nodes: dijkstra的结果信息表
+    :param paths: [{"path", "cost"}]
+    :param stack: 记住当前走过的节点
+    """
+    stack.append(source)
+    if source == destination:
+        paths.append(stack[:])
+        stack.pop(-1)  # 弹出自己
+        return None
+
+    for father in nodes[source]["fathers"]:
+        get_paths(nodes, paths, stack, father, destination)
+
+    stack.pop(-1)
+
+
+def get_cost(path: List[int], cost: List[List[int]]) -> int:
+    """
+    给定一条路径求这条路径的花费
+    :param cost: 每条边的花费
+    :param path: 路径
+    """
+    result = 0
+    for a, b in zip(path[:-1], path[1:]):
+        result += cost[a][b]
+    return result
+
+
 def summit():
-    pass
+    # cityNum城市（点）的数量,
+    # roadNum道路（边）数量,
+    # source, 起点
+    # destination,终点
+    cityNum, roadNum, source, destination = map(int, input().split())
+    source, destination = destination, source
+    # 邻接矩阵表示法,边的长度为0表示无法到达
+    # 边的长度
+    graph = [[0] * cityNum for _ in range(cityNum)]
+    # 边的花费
+    cost = [[0] * cityNum for _ in range(cityNum)]
+    # 读取边
+    for _ in range(roadNum):
+        a, b, d, c = map(int, input().split())
+        graph[a][b] = d
+        graph[b][a] = d
+        cost[a][b] = c
+        cost[b][a] = c
+        
+    # 维护dijkstra求最短路劲时所有节点的父亲
+    nodes = [{
+        "fathers": [],
+        "index": idx,
+        "weight": float("inf")  # 累计路径长度
+    } for idx in range(cityNum)]
+
+    nodes[source]["weight"] = 0
+    unSelectedNodes = nodes[:]
+    while len(unSelectedNodes) != 0:  # dijkstra
+        node = min(unSelectedNodes, key=lambda x: x["weight"])
+        for idx, weight in enumerate(graph[node["index"]]):
+            # 存在一条路径
+            if weight != 0:
+                # 找到一条更短的路径
+                if weight + node["weight"] < nodes[idx]["weight"]:
+                    nodes[idx]["weight"] = weight + node["weight"]
+                    nodes[idx]['fathers'] = [node["index"]]
+                # 跟原来路径的长度相同
+                elif weight + node["weight"] == nodes[idx]["weight"]:
+                    nodes[idx]['fathers'].append(node["index"])
+        unSelectedNodes.remove(node)
+
+    # 存放source到destination的所有最短路径
+    paths = []
+    # 其实就是个dfs
+    source, destination = destination, source
+    get_paths(nodes, paths, [], source, destination)
+    # 多条最短路径，选择最便宜的
+    result = min(paths, key=lambda x: get_cost(x, cost))
+    # 输出结果
+    print(f"{' '.join(map(str, result))} {nodes[source]['weight']} {get_cost(result, cost)}")
 
 
 if __name__ == '__main__':

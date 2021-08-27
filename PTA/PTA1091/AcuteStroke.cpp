ee1fe2be90b76599e478da20e9961eacc5d3f2ba
ednow <3334300267@qq.com>
2021-08-27 22:55:52 +0800

    pta1091.cpp debugging

--- PTA/PTA1091/AcuteStroke.cpp
+++ PTA/PTA1091/AcuteStroke.cpp
@@ -24,65 +24,66 @@ struct item{
     int z{};
 };
 
-void
+int
 bfs(int z, int y, int x,
     int &M, int &N, int &L,
-    vector<vector<vector<int>>> &slices, map<int, bool> &isVisited, vector<int> &path){
-    isVisited[z*N*M + y*N + x] = true;
+    vector<vector<vector<int>>> &slices, vector<vector<vector<int>>> &isVisited){
+    isVisited[z][y][x] = true;
     queue<item> q;
     q.push(item{.x=x, .y=y, .z=z});
+    int w = 0;
     while (!q.empty()){
         auto head = q.front();
         x = head.x;
         y = head.y;
         z = head.z;
 
-        path.push_back(z*N*M + y*N + x);
+        w++;
         if (x+1 < N){
-            if (slices[z][y][x+1] and !isVisited[z*N*M + y*N + x + 1]){
-                isVisited[z*N*M + y*N + x+1] = true;
+            if (slices[z][y][x+1] and !isVisited[z][y][x+1]){
+                isVisited[z][y][x+1] = true;
                 q.push(item{.x=x + 1, .y=y, .z=z});
             }
         }
 
         if (x-1>=0){
-            if (slices[z][y][x-1]and !isVisited[z*N*M + y*N + x - 1]){
-                isVisited[z*N*M + y*N + x-1] = true;
+            if (slices[z][y][x-1]and !isVisited[z][y][x-1]){
+                isVisited[z][y][x-1] = true;
                 q.push(item{.x=x - 1, .y=y, .z=z});
             }
         }
 
         if (y+1 < M){
-            if (slices[z][y+1][x] and !isVisited[z*N*M + (y+1)*N + x]){
-                isVisited[z*N*M + (y+1)*N + x] = true;
+            if (slices[z][y+1][x] and !isVisited[z][y+1][x]){
+                isVisited[z][y+1][x] = true;
                 q.push(item{.x=x, .y=y+1, .z=z});
             }
         }
 
         if (y-1>=0){
-            if (slices[z][y-1][x] and !isVisited[z*N*M + (y-1)*N + x]){
-                isVisited[z*N*M + (y-1)*N + x] = true;
+            if (slices[z][y-1][x] and !isVisited[z][y-1][x]){
+                isVisited[z][y-1][x] = true;
                 q.push(item{.x=x, .y=y-1, .z=z});
             }
         }
 
         if (z+1 < L){
-            if (slices[z+1][y][x] and !isVisited[(z+1)*N*M + y*N + x]){
-                isVisited[(z+1)*N*M + y*N + x] = true;
+            if (slices[z+1][y][x] and !isVisited[z+1][y][x]){
+                isVisited[z+1][y][x] = true;
                 q.push(item{.x=x, .y=y, .z=z+1});
             }
         }
 
         if (z-1>=0){
-            if (slices[z-1][y][x] and !isVisited[(z-1)*N*M + y*N + x]){
-                isVisited[(z-1)*N*M + y*N + x] = true;
+            if (slices[z-1][y][x] and !isVisited[z-1][y][x]){
+                isVisited[z-1][y][x] = true;
                 q.push(item{.x=x, .y=y, .z=z-1});
             }
         }
 
         q.pop();
     }
-
+    return w;
 }
 
 int
@@ -107,21 +108,14 @@ MAIN(){
     // 区域的重量
     int weight{};
     // 某个节点有没有被访问过
-    map<int, bool> isVisited;
-    while (!isNotVisited.empty()){
-        vector<int> path;
-        auto label = *isNotVisited.begin();
-        int z = label / (N * M);
-        int y = (label - z*N*M) / N;
-        int x = label % N;
-        bfs(z, y, x, M, N, L, slices, isVisited, path);
-
-        if (path.size()>=T){
-            weight += (int)path.size();
-        }
-
-        for (auto &a:path) {
-            isNotVisited.erase(a);
+    vector<vector<vector<int>>> isVisited(L, vector<vector<int>>(M, vector<int>(N, 0)));
+    for (int i = 0; i < L; ++i) {
+        for (int j = 0; j < M; ++j) {
+            for (int k = 0; k < N; ++k) {
+                if (slices[i][j][k] and !isVisited[i][j][k]){
+                    weight+=bfs(i, j, k, M, N, L, slices, isVisited);
+                }
+            }
         }
     }
 

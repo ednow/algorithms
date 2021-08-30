a9ac23848ecd248fb9c99ad8ad01fda21f1c824e
ednow <3334300267@qq.com>
2021-08-30 18:50:50 +0800

    pta1085.cpp ac

--- PTA/PTA1085/PerfectSequence.cpp
+++ PTA/PTA1085/PerfectSequence.cpp
@@ -15,56 +15,30 @@
 #include "set"
 #include "limits"
 #include "algorithm"
-#include "queue"
-
-struct item{
-    int low{};
-    int high{};
-};
-
 using namespace std;
-
-void bfs(vector<int> &nums, int &maxSize, int&p) {
-    queue<item> q;
-    q.push(item{.low=0, .high=(int)nums.size() - 1});
-
-    while (!q.empty()){
-        auto head = q.front();
-        if (head.high < head.low) {
-            q.pop();
-            continue;
-        }
-
-        if (nums[head.low] * p >= nums[head.high]){
-            if (head.high - head.low + 1 > maxSize){
-                maxSize = head.high - head.low + 1;
-            }
-            // 不用push进队列里面，因为此时已经是最大的了
-        }else{
-            q.push(item{.low=head.low + 1, .high=head.high});
-            q.push(item{.low=head.low, .high=head.high - 1});
-        }
-
-        q.pop();
-    }
-}
-
 
 int
 MAIN(){
-    int numLen{}, p{};
+    long long numLen{}, p{};
     cin >> numLen >> p;
-    vector<int> nums;
+    vector<long long> nums;
     nums.reserve(numLen);
-    int num{};
-    for (int i = 0; i < numLen; ++i) {
+    long long num{};
+    for (long long i = 0; i < numLen; ++i) {
         cin >> num;
         nums.push_back(num);
     }
 
     sort(nums.begin(), nums.end());
-    int maxSize{};
-    bfs(nums, maxSize, p);
+    long long maxSize{};
+    for (long long i = 0; i < numLen; ++i) {
+        // i控制左指针
+        // maxSize控制右指针
+        while (i + maxSize < numLen and nums[i + maxSize] <= nums[i] * p) {
+            maxSize++;
+        }
+    }
+
     cout << maxSize;
 
     return 0;
--- PTA/PTA1085/README.md
+++ PTA/PTA1085/README.md
@@ -11,8 +11,14 @@
 
 ## 边界点
 
+卡内存，
+int最大表示10位10进制位
+
 ## 思路
-删掉最小的元素，或者删掉最大的元素
+<!-- 删掉最小的元素，或者删掉最大的元素 -->
+
+直接查找遍历：[文献](https://blog.csdn.net/whutshiliu/article/details/82913133)
+
 
 ## cpp
 ### 第一次提交
@@ -28,8 +34,12 @@
 bfs+二分
 
 答案错误+内存超限
++ 答案错误是因为数据类型的大小
 
 <details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210830182928.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210830182928.png';" /></details>
+
+### 第四次提交
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210830184955.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210830184955.png';" /></details>
 
 ### 语法技术
 #### 得到集合中最后一个元素

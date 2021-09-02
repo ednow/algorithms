9ccb221538d25fe8c1f1946a3830ee300f8d5beb
ednow <3334300267@qq.com>
2021-09-03 00:21:53 +0800

    pta1101.cpp 第一次提交

--- PTA/PTA1101/QuickSort.cpp
+++ PTA/PTA1101/QuickSort.cpp
@@ -13,6 +13,8 @@
 #include "vector"
 #include "map"
 #include "iostream"
+#include "algorithm"
+
 
 using namespace std;
 
@@ -24,15 +26,56 @@ MAIN(){
     for (int i = 0; i < N; ++i) {
         cin >> nums[i];
     }
-    // 存放[i,j]内的最小和最大值
-    // first为最小值,second为最大值
-    vector<vector<pair<int, int>>> dp(N, vector<pair<int, int>>(N));
+    // 存放[0,i]之间最大的数
+    vector<int> maxZeroToI(N);
+    maxZeroToI[0] = nums[0];
+    // 第一次dp,求最大的数
+    for (int i = 1; i < N; ++i) {
+        if (maxZeroToI[i-1] < nums[i]) {
+            maxZeroToI[i] = nums[i];
+        } else {
+            maxZeroToI[i] = nums[i-1];
+        }
+    }
 
+    // 存放[i,N]之间最小的数
+    vector<int> minIToN(N);
+    minIToN[N-1] = nums[N-1];
+    for (int i = N-2; i >= 0; --i) {
+        if (minIToN[i+1] > nums[i]) {
+            minIToN[i] = nums[i];
+        } else {
+            minIToN[i] = nums[i+1];
+        }
+    }
     // 顺次找到最小值和最大值
 
     // 判断k是不是做过枢纽
-    // dp[0][k-1].second > nums[k],(左边的最大值也小于k位置的数)
-    // dp[k+1][N].first < nums[k],(右边的最小值也大于k位置的数)
+    // dp[0][k-1].second < nums[k],(左边的最大值也小于k位置的数)
+    // dp[k+1][N].first > nums[k],(右边的最小值也大于k位置的数)
+    vector<int> result;
+    result.reserve(N);
+    for (int i = 1; i < N-1; ++i) {
+        if (maxZeroToI[i - 1] < nums[i] and minIToN[i + 1] > nums[i]) {
+            result.push_back(nums[i]);
+        }
+    }
+    // 边界单独判断
+    if (minIToN[1]>nums[0]) {
+        result.push_back(nums[0]);
+    }
+
+    if (maxZeroToI[N-2] < nums[N-1]) {
+        result.push_back(nums[N - 1]);
+    }
+    sort(result.begin(), result.end());
+    cout << result.size() << endl;
+    auto iter = result.begin();
+    cout << *iter++;
+    while (iter != result.end()) {
+        cout << " " << *iter++;
+    }
+    cout << endl;
     return 0;
 }
 
--- PTA/PTA1101/README.md
+++ PTA/PTA1101/README.md
@@ -16,5 +16,8 @@ partition:划分
 动态规划
 
 ## cpp
+### 第一次提交
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210903002119.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210903002119.png';" /></details>
+
 
 ### 语法技术

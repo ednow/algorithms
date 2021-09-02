2fd41cc10dafe6fb03923e9b0fed6b055906739a
ednow <3334300267@qq.com>
2021-09-02 15:18:05 +0800

    pta1153.cpp 第一次提交

---
+++ PTA/PTA1153/DecodeRegistrationCardOfPAT.cpp
@@ -0,0 +1,122 @@
+//
+// Created by ednow on 2021/9/2.
+//
+
+#define CASEID 1153
+
+#ifdef CASEID
+#define MAIN PTA1153
+#else
+#define MAIN main
+#endif
+
+#include "iostream"
+#include "map"
+#include "vector"
+#include "algorithm"
+
+using namespace std;
+
+struct Site{
+    int siteId{};
+    int testNum{};
+    int totalScore{};
+};
+
+struct Card{
+    string card{};
+    int score{};
+};
+
+int
+MAIN(){
+    int queryNum{}, cardNum{};
+    cin >> cardNum >> queryNum;
+    map<char, vector<Card>> cardsByLevel;
+    map<int, map<int, int>> testNumByDateAndSite;
+    map<int, Site> numAndTotalScoreBySiteId;
+    while (cardNum--) {
+        string card{};
+        int score{};
+        cin >> card >> score;
+        int siteId{},date{},testId{};
+        sscanf(card.substr(1,card.size()-1).c_str(), "%3d%6d%3d", &siteId, &date, &testId);
+        cardsByLevel[card[0]].push_back(Card{.card=card, .score=score});
+        testNumByDateAndSite[date][siteId]++;
+        numAndTotalScoreBySiteId[siteId].testNum++;
+        numAndTotalScoreBySiteId[siteId].totalScore+=score;
+    }
+    map<int, vector<Site>> siteByDate;
+    for (auto &numByDateAndSite:testNumByDateAndSite) {
+        for (auto &numBySite:numByDateAndSite.second) {
+            siteByDate[numByDateAndSite.first].push_back(Site{.siteId=numBySite.first, .testNum=numBySite.second});
+        }
+    }
+    for (auto&item:cardsByLevel) {
+        sort(item.second.begin(), item.second.end(), [](const auto &a, const auto &b) {
+            if (a.score != b.score) return a.score > b.score;
+            return a.card < b.card;
+        });
+    }
+
+    for (auto&item:siteByDate) {
+        sort(item.second.begin(), item.second.end(), [](const auto &a, const auto &b) {
+            if (a.testNum != b.testNum) return a.testNum > b.testNum;
+            return a.siteId < b.siteId;
+        });
+    }
+
+    for (int i = 1; i < queryNum+1; ++i) {
+        int queryType;
+        cin >> queryType;
+        // 输出对应等级的结果
+        if (queryType == 1) {
+            string level;
+            cin >> level;
+            cout << "Case " << i << ": " << queryType << " " << level<< endl;
+            if (cardsByLevel.find(level[0]) == cardsByLevel.end()) {
+                cout << "NA"<<endl;
+                continue;
+            }
+            for (auto & item:cardsByLevel[level[0]]) {
+                cout << item.card << " " <<item.score << endl;
+            }
+            continue;
+        }
+        // 输出每一个考试站的情况
+        if (queryType == 2) {
+            int siteId;
+            cin >> siteId;
+            cout << "Case " << i << ": " << queryType << " " << siteId<< endl;
+            if (numAndTotalScoreBySiteId.find(siteId)==numAndTotalScoreBySiteId.end()) {
+                cout << "NA"<<endl;
+                continue;
+            }
+            cout << numAndTotalScoreBySiteId[siteId].testNum << " " << numAndTotalScoreBySiteId[siteId].totalScore << endl;
+            continue;
+        }
+        // 输出每一个日期中，考试站和他提交的题目的数量
+        if (queryType==3) {
+            int date;
+            cin >> date;
+            cout << "Case " << i << ": " << queryType << " " << date<< endl;
+            if (siteByDate.find(date)==siteByDate.end()) {
+                cout << "NA"<<endl;
+                continue;
+            }
+            for (auto & item:siteByDate[date]) {
+                cout << item.siteId << " " << item.testNum << endl;
+            }
+        }
+
+    }
+    return 0;
+}
+
+
+#ifdef CASEID
+#include "utils.h"
+TEST(PTA, CASEID){
+    DO_TEST(CASEID)
+}
+#endif
---
+++ PTA/PTA1153/README.md
@@ -0,0 +1,27 @@
+# PTA总结
+## 考点
++ 
+
+
+## 问题重述
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210902140548.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210902140548.png';" /></details>
+
+
+### 要点复述
+
+## 边界点
+
+## 思路
+
+## cpp
+
+### 第一次提交
+<details><summary>图片详情</summary><img src="https://raw.githubusercontent.com/ednow/cloudimg/main/githubio/20210902150714.png" alt="找不到图片(Image not found)" onerror="this.onerror=null;this.src='https://gitee.com/ednow/cloudimg/raw/main/githubio/20210902150714.png';" /></details>
+
+
+### 语法技术
+#### 判断map中存不存在key
+find
+
+[参考文献](https://stackoverflow.com/questions/1939953/how-to-find-if-a-given-key-exists-in-a-c-stdmap)
+
